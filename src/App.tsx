4087dacffe68a5e224b9cd0d2aec8ec2dab16711
Fiterman <gfiterman96@gmail.com>
2023-11-19 16:45:49 -0300

    chore: add redux to application apply to simple counter example

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,40 @@
+// src/components/App.tsx
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { increment, decrement } from './redux/slices/counterSlice';
 import CountDisplay from './CountDisplay';
 
-type State = {
-    currentCount: number;
-};
+function App() {
+    const dispatch = useDispatch();
+    const currentCount = useSelector((state:any) => state.counter.currentCount);
 
-type Props = {};
-
-class App extends React.Component<Props, State> {
-    state: State = {
-        currentCount: 0,
+    const handleIncrement = () => {
+        dispatch(increment());
     };
 
-    handleIncrement = () => {
-        this.setState((prevState) => ({
-            currentCount: prevState.currentCount + 1,
-        }));
+    const handleDecrement = () => {
+        dispatch(decrement());
     };
 
-    handleDecrement = () => {
-        this.setState((prevState) => ({
-            currentCount: prevState.currentCount - 1,
-        }));
-    };
-
-    render() {
-        return (
-            <div className='text-center bg-rose-950 h-[100vh] pt-16'>
-                <div className='flex items-center justify-center w-[100vw] gap-x-4 mb-10'>
-                    <button onClick={this.handleIncrement} className='text-4xl bg-green-700 p-2 rounded-2xl caret-purple-5 text-white font-bold'>+</button>
-                    <button onClick={this.handleDecrement} className='text-4xl bg-red-700 p-2 rounded-2xl caret-purple-5 text-white font-bold'>-</button>
-                </div>
-                <CountDisplay currentCount={this.state.currentCount} />
+    return (
+        <div className='text-center bg-rose-950 h-[100vh] pt-16'>
+            <div className='flex items-center justify-center w-[100vw] gap-x-4 mb-10'>
+                <button
+                    onClick={handleIncrement}
+                    className='text-4xl bg-green-700 p-2 rounded-2xl caret-purple-5 text-white font-bold'
+                >
+                    +
+                </button>
+                <button
+                    onClick={handleDecrement}
+                    className='text-4xl bg-red-700 p-2 rounded-2xl caret-purple-5 text-white font-bold'
+                >
+                    -
+                </button>
             </div>
-        );
-    }
+            <CountDisplay currentCount={currentCount} />
+        </div>
+    );
 }
 
 export default App;
